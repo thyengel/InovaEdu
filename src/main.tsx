b02192c2e1f70f983410bb9ce7c76326e2c16b4d
Thyelen Engel <thyengel@gmail.com>
2025-10-14 13:00:45 -0300

    install prettier and eslint

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
-import { Provider } from "@/components/ui/provider"
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App"
+import { Provider } from "@/components/ui/provider";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -9,4 +9,4 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <App />
     </Provider>
   </React.StrictMode>,
-)
+);
