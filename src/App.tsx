0d1437c688eecc2cb8b20825b24c5d8e6b50d192
Thyelen Engel <130775295+thyengel@users.noreply.github.com>
2025-10-16 11:27:05 -0300

    Adicionei logo e criei o link para cadastro (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
           // background: 'linear-gradient(180deg,rgba(0, 0, 0, 1) 0%, rgba(0,0,0, 0.8) 80%, rgba(40, 120, 32, 0.78) 100%)'
           // background: 'radial-gradient(circle,rgba(0, 0, 0, 1) 0%, rgba(0,0,0, 0.8) 80%, rgba(40, 120, 32, 0.78) 100%)'
           background:
-            "radial-gradient(1200px 600px at 10% 20%, rgba(15,76,107,0.08), transparent 12%), radial-gradient(900px 500px at 85% 80%, rgba(69,42,106,0.06), transparent 10%), linear-gradient(180deg, #050507 0%, #061605ff 100%)",
+            "radial-gradient(1200px 600px at 10% 20%, rgba(15,76,107,0.08), transparent 12%), radial-gradient(900px 500px at 85% 80%, rgba(70, 39, 115, 0.14), transparent 10%), linear-gradient(180deg, #050507 0%, #061605ff 100%)",
         }}
       >
         <Login />
