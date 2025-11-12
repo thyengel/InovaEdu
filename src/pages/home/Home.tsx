f45bfd52f0be10eb082e9e92233c02b717545d35
Thyelen Engel <130775295+thyengel@users.noreply.github.com>
2025-11-12 20:50:31 -0300

    adicionei imagens (#8)

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -33,17 +33,17 @@ const LEARNING_TRACK = [
   {
     title: "Criadores do Futuro ",
     description: "Tecnologia & Inovação",
-    image: "https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+    image: "https://i.postimg.cc/28QXq81V/Whats-App-Image-2025-11-12-at-20-29-23.jpg"
   },
   {
     title: "Comunicação e Expressão",
     description: "Mídia & Criatividade",
-    image: "https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+    image: "https://i.postimg.cc/fRvFPTZd/Whats-App-Image-2025-11-12-at-20-29-23-1.jpg"
   },
   {
     title: "Vida e Carreira",
     description: "Autodesenvolvimento & Habilidades para o Futuro",
-    image: "https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+    image: "https://i.postimg.cc/2STcwcFD/Whats-App-Image-2025-11-12-at-20-29-24.jpg"
   }
 ]
 
